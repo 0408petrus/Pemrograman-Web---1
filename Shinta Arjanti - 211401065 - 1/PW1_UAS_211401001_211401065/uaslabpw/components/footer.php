95849288c422bfb116454f8fa114227d672014bb
Shinta Arjanti <shintaarjanti@students.usu.ac.id>
2022-12-28 00:06:12 +0700

    PW1_UAS_211401001_211401065

---
+++ Shinta Arjanti - 211401065 - 1/PW1_UAS_211401001_211401065/uaslabpw/components/footer.php
@@ -0,0 +1,41 @@
+<!-- Link CSS Eksternal  -->
+<link rel="stylesheet" href="css/style.css">
+
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +62 811 609 9903 </p>
+         <p> <i class="fas fa-phone"></i> +62 823 7032 3438 </p>
+         <p> <i class="fas fa-envelope"></i> shintaarjanti@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> medan, sumatera utara - 20222</p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/shinta.arjanti" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/shintaarjanti_" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/segin28/" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>ss resto</span> | all rights reserved! </p>
+
+</footer>
